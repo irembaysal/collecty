a34d04b84fa36c255aea02282fe23ffcecfcae48
Irem Baysal <irembaysal@Irem-MacBook-Air.local>
2016-12-31 13:38:15 +0300

    create project

---
+++ src/main/java/com/collecty/CollectyApplication.java
@@ -0,0 +1,12 @@
+package com.collecty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/collecty/CollectyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.collecty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollectyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
